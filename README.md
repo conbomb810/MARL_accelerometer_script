1f175e47aa241ef471c3cc4b99e619ce3b4951f9
conbomb810 <89608409+conbomb810@users.noreply.github.com>
2025-01-14 14:19:23 -0500

    accelerometer_plotter.py updates
    the script now works through a given directory that can be changed in one line. README.md has been updated to show how to change it. Opacity of the line in the plots has been changed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MARL_accelerometer_script
  Reads data from an accelerometer text file and converts it to a plot as an image
+
+ To change which directory is worked in, edit line 44 to the desred path
+ Please note that python doesn't like the use of "\" in strings, so I used "//" instead
--- accelerometer_plotter.py
+++ accelerometer_plotter.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 #Read the data of a file and format the time, x and z columns into numpy arrays
 def readData(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
-    #Extract trial name
+    #Get the trial name
     trial_lines = [line for line in lines if line.startswith("Trial:")]
     trial_name = trial_lines[1].split("\t")[1].strip().replace(".txt", "") if len(trial_lines) > 1 else "Unknown"
 
     #Locate the data section
     data_start = next(i for i, line in enumerate(lines) if line.startswith("Unix Time")) + 1
 
-    #Read the data lines
+    #Read the data lines and split the entries with tabs
     data = [line.strip().split('\t') for line in lines[data_start:]]
 
     #Convert the data to numpy arrays
@@ -26,7 +27,7 @@ def readData(filename):
 def plotData(x, z, trial_name):
     plt.figure(figsize=(8, 6))
     #plt.scatter(x, z, c='blue', alpha=0.7, edgecolors='k', label="Data Points")
-    plt.plot(x, z, c='red', alpha=0.7, label="Line Connection")
+    plt.plot(x, z, c='red', alpha=1, label="Line Connection")
     plt.title(f"{trial_name}")
     plt.xlabel("X-axis")
     plt.ylabel("Z-axis")
@@ -35,8 +36,18 @@ def plotData(x, z, trial_name):
     plt.savefig(trial_name + ".png")
     plt.close()
 
-#Example usage
-filename = "test_accelerometer_2.txt"
-trial_name, x, z = readData(filename)
+    return 0
 
-plotData(x, z, trial_name)
+
+def main():
+    #Change the following line to change the directory this script is worked in
+    directory = "C://Users//conbo//Documents//GitHub//MARL_accelerometer_script"
+
+    os.chdir(directory)
+    for file in os.listdir():
+        if ".txt" in file.lower() and "accelerometer" in file.lower():
+            print(file)
+            trial_name, x, z = readData(file)
+            plotData(x, z, trial_name)
+
+main()
